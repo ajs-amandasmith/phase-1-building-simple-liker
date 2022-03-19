5ad55c867a8bc63f5f68e70f83716cc9ecb6e4ed
Amanda Smith <ajs.amandasmith@gmail.com>
2022-03-18 21:47:26 -0500

    commit tree ad6a41443b1005dfe580ea5b10bcd31e05174fb0

--- main.js
+++ main.js
@@ -4,8 +4,91 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// user clicks on an empty heart
+  // invoke the given function (mimicServer) to simulate making a server request
+  // when the server returns a failure status
+    // respond to the error using a .catch(() => ()) block after .then(() => {}) block
+    // display the error modal by removing the hidden class
+    // display the server error message in the modal
+    // use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+  // when the server returns a success status
+    // change the heart to a full heart
+    // add the .activated-heart class to make the heart appear red
+// user clicks on a full heart
+  // change the heart back to an empty heart
+  // remove the .activated-heart class
 
+// document.addEventListener("DOMContentLoaded", () => {
+  
+//   function clickHeart() {
+//   let hearts = document.getElementsByClassName('like-glyph');
+//     for (const heart of hearts) {
+//       heart.addEventListener("click", clicky)
+//     }
+//   }
 
+//   function clicky(hearts) {
+//     console.log(hearts.target);
+//     mimicServerCall()
+//       .then( () => {
+//         if (hearts.target.innerText === EMPTY_HEART) {
+//           hearts.target.className.add('activated-heart');
+//           hearts.target.innerText = FULL_HEART;
+//         } else {
+//           hearts.target.className.remove('activated-heart');
+//           hearts.target.innerText = EMPTY_HEART;
+//         }
+
+//       })
+//       .catch(() => {
+//         const error = document.getElementById('modal');
+//         console.log(error);
+//         error.className='';
+//       })
+        
+//       .then (() => { setTimeout( () => {
+//         const newError = document.getElementById('modal');
+//         newError.className ='hidden',[ 6000]
+//         })
+//       })
+//     }
+
+//   clickHeart();
+
+// })
+
+let heartsNodeArray = [...document.getElementsByClassName('like-glyph')];
+let modal = document.getElementById('modal');
+let modalParagraph = document.getElementById('modal-message');
+
+let callServerAndCatch = (event) => {
+  mimicServerCall()
+  .then(() => handleResponse(event))
+  .catch(error => handleError(error))
+}
+
+let handleError = (errorMessage) => {
+  modal.classList.remove('hidden')
+  modalParagraph.innerText = errorMessage;
+  setTimeout(() => { 
+    modal.classList.add('hidden')
+    modalParagraph.innerText = '';
+  }, 3000);
+}
+
+let handleResponse = (event) => {
+  if (event.target.textContent === EMPTY_HEART) {
+    event.target.classList.add('activated-heart');
+    event.target.textContent = FULL_HEART;
+  } else {
+    event.target.classList.remove('activated-heart');
+    event.target.textContent = EMPTY_HEART;
+  }
+}
+
+heartsNodeArray.map(heartNode => {
+  heartNode.addEventListener('click', callServerAndCatch)
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
